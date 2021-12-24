814eafdb16a269417865a82acf1f24f0c2d886ef
n3ki4 <nik.flyme.criachko@gmail.com>
2021-12-24 13:06:38 +0200

    finished device controller

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,17 +1,26 @@
 const uuid = require('uuid')
-const { Device } = require('../models/models')
+const { Device, DeviceInfo } = require('../models/models')
 const ApiError = require('../error/ApiError')
 const path = require('path')
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            const { name, price, brandId, typeId, info } = req.body
+            let { name, price, brandId, typeId, info } = req.body
             const { img } = req.files
             let fileName = uuid.v4() + ".jpg"
+            const device = await Device.create({ name, price, brandId, typeId, img: fileName })
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
-            const device = await Device.create({ name, price, brandId, typeId, img: fileName })
+            if (info) {
+                info = JSON.parse(info)
+                info.foreach(i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    }))
+            }
             return res.json(device)
         } catch (e) {
             next(ApiError.badRequest(e.message))
@@ -29,7 +38,7 @@ class DeviceController {
             devices = await Device.findAndCountAll({ limit, offset })
         }
         if (!typeId && brandId) {
-            devices = await Device.findfindAndCountAll({
+            devices = await Device.findAndCountAll({
                 where: { brandId },
                 limit,
                 offset
@@ -54,7 +63,12 @@ class DeviceController {
 
     }
     async getOne(req, res) {
-
+        const { id } = req.params
+        const device = await Device.findOne({
+            where: { id },
+            include: [{ model: DeviceInfo, as: 'info' }]
+        })
+        return res.json(device)
     }
 
     async delete(req, res) {
