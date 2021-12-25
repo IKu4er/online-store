494f50b5fd2322b5c2e98603e9c0f579a6932418
n3ki4 <nik.flyme.criachko@gmail.com>
2021-12-25 10:39:31 +0200

    added AppRouter and NavBar, refactored a bit

--- front-end/src/pages/Shop.js
+++ front-end/src/pages/Shop.js
@@ -1,17 +1,17 @@
-import React, {useContext, useEffect} from 'react';
-import {Container} from "react-bootstrap";
+import React, { useContext, useEffect } from 'react';
+import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import TypeBar from "../components/TypeBar";
-import BrandBar from "../components/BrandBar";
-import DeviceList from "../components/DeviceList";
-import {observer} from "mobx-react-lite";
-import {Context} from "../index";
-import {fetchBrands, fetchDevices, fetchTypes} from "../http/deviceAPI";
-import Pages from "../components/Pages";
+import TypeBar from "../commponents/TypeBar"
+import BrandBar from "../commponents/BrandBar";
+import DeviceList from "../commponents/DeviceList";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+import { fetchBrands, fetchDevices, fetchTypes } from "../http/deviceAPI";
+import Pages from "../commponents/Pages";
 
 const Shop = observer(() => {
-    const {device} = useContext(Context)
+    const { device } = useContext(Context)
 
     useEffect(() => {
         fetchTypes().then(data => device.setTypes(data))
@@ -33,12 +33,12 @@ const Shop = observer(() => {
         <Container>
             <Row className="mt-2">
                 <Col md={3}>
-                    <TypeBar/>
+                    <TypeBar />
                 </Col>
                 <Col md={9}>
-                    <BrandBar/>
-                    <DeviceList/>
-                    <Pages/>
+                    <BrandBar />
+                    <DeviceList />
+                    <Pages />
                 </Col>
             </Row>
         </Container>
